2e426fcd3922d3a049ff666d8068820624d3f786
smakosheik <smakosheik@gmail.com>
2022-08-05 15:29:44 +0530

    done my coding

--- src/org/git/exProject.java
+++ src/org/git/exProject.java
@@ -7,6 +7,24 @@ public static void main(String[] args) {
 	
 
 		
+	int xx=123;
+	System.out.println(xx);
+	//
+	//
+	//
+	///
+	///
+	///
+	///
+	///
+	///
+	//
+	///
+	
+	////
+	///
+	
+	
 		System.out.println("smakosheik");
 	
 	}
